d2b49448fff447ed03bf307f98e634ab0d1c8435
hustmonk <liujingminghust@163.com>
2015-07-15 17:50:33 +0800

    init code

--- shell/build.sh
+++ shell/build.sh
@@ -3,17 +3,19 @@
 # Last modified: 
 
 set -e
-#python highuser.py
+python courseStatisticTime.py
+python highuser.py
 
-#python module.py
-#python lastday.py
-#python objweight.py
-#python transfer.py
+python module.py
+python lastday.py
+python objweight.py
+python transfer.py
 python statistic.py
 python moreinfo.py
 
-#python lastdayfeature.py
-#python alldayfeature.py
-#python day_level_feafure.py
-#python wholesitefeature.py
+python lastdayfeature.py
+python alldayfeature.py
+python day_level_feafure.py
+python wholesitefeature.py
+
 sh run.sh
